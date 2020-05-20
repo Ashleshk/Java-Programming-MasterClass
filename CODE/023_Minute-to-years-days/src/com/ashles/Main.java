8f8e8b35693f4d449125b093d219f3ad51c73e21
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-05-20 10:58:03 +0530

    Minutes-to-year & Inequity Calculator

---
+++ CODE/023_Minute-to-years-days/src/com/ashles/Main.java
@@ -0,0 +1,11 @@
+package com.ashles;
+
+public class Main {
+    public static void main(String[] args) {
+        MinutesToYearsDaysCalculator.printYearsAndDays(525600);
+        MinutesToYearsDaysCalculator.printYearsAndDays(1051200);
+        MinutesToYearsDaysCalculator.printYearsAndDays(561600);
+    }
+
+
+}
---
+++ CODE/024_Inequity-calculator/src/com/ashlesh/Main.java
@@ -0,0 +1,10 @@
+package com.ashlesh;
+
+public class Main {
+    public static void main(String[] args) {
+        IntEqualityPrinter.printEqual(1, 1, 1);
+        IntEqualityPrinter.printEqual(1, 1, 2);
+        IntEqualityPrinter.printEqual(-1, -1, -1);
+        IntEqualityPrinter.printEqual(1, 2, 3);
+    }
+}
