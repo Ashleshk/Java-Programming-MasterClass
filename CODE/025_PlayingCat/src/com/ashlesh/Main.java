11b68ddcdfc2ed49eef843a3e98d249c8cf221d2
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-05-20 11:00:28 +0530

    Playing Cat & Switch Statement

---
+++ CODE/025_PlayingCat/src/com/ashlesh/Main.java
@@ -0,0 +1,9 @@
+package com.ashlesh;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(playingCat.isCatPlaying(true, 10));
+        System.out.println(playingCat.isCatPlaying(false, 36));
+        System.out.println(playingCat.isCatPlaying(false, 35));
+    }
+}
