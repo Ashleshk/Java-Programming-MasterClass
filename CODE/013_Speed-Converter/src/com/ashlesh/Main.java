479f10ed6f6a30856d055b47dfb5373e693db59d
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-05-20 10:35:32 +0530

    Practice Problems
    - Speed Converter
    - MegaBytesConerter
    - Barking Dogs
    - Leap Year

---
+++ CODE/013_Speed-Converter/src/com/ashlesh/Main.java
@@ -0,0 +1,11 @@
+package com.ashlesh;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double miles = SpeedConverter.toMilesPerHour(10.5);
+        System.out.println("Miles = " + miles);
+
+        SpeedConverter.printConversion(10.5);
+    }
+}
---
+++ CODE/014_MegaBytesConverter/src/com/ashlesh/Main.java
@@ -0,0 +1,9 @@
+package com.ashlesh;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
+        MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
+    }
+}
