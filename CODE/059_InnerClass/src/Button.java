d0d5f0fd1c3b5d8739350c12eb2be5ef449dfbf0
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-05-21 11:54:22 +0530

    Visited Places _Arraylist & innerclass

---
+++ CODE/059_InnerClass/src/Button.java
@@ -0,0 +1,25 @@
+public class Button {
+    private String title;
+    private OnClickListener onClickLister;
+
+    public Button(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setOnClickListener(OnClickListener onClickListener) {
+        this.onClickLister = onClickListener;
+    }
+
+    public void onClick() {
+        this.onClickLister.onClick(this.title);
+    }
+
+    public interface OnClickListener {
+        public void onClick(String title);
+    }
+
+}
