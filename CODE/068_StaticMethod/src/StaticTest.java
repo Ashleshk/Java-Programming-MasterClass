db19c3ed5191a13782fd8edbe0a1ac2e0d146384
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-05-21 11:57:37 +0530

    Static Method & Final, Static Initializers

---
+++ CODE/068_StaticMethod/src/StaticTest.java
@@ -0,0 +1,17 @@
+public class StaticTest {
+    private static int numInstances = 0;
+    private String name;
+
+    public StaticTest(String name) {
+        this.name = name;
+        numInstances++;
+    }
+
+    public static int getNumInstances() {
+        return numInstances;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ CODE/069_Final-&-StaticInitialzers/src/ExtendedPassword.java
@@ -0,0 +1,13 @@
+public class ExtendedPassword extends Password {
+    private int decryptedPassword;
+
+    public ExtendedPassword(int password) {
+        super(password);
+        this.decryptedPassword = password;
+    }
+
+//    @Override
+//    public void storePassword() {
+//        System.out.println("Saving password as " + this.decryptedPassword);
+//    }
+}
