e2e979388aa1863e25b7fe01240e4793ed7c7ccb
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-05-22 13:47:42 +0530

    Tilted Pane, GridPane

---
+++ JavaFX/GridPane-Layout/src/module-info.java
@@ -0,0 +1,6 @@
+module GridPane.Layout {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+}
---
+++ JavaFX/hello world/src/module-info.java
@@ -0,0 +1,6 @@
+module helloworldFX {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+}
