7de343b2cec55853a329f1252333373e2e94ad3c
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-05-22 13:50:41 +0530

    Thread & Runnable

---
+++ JavaFX/ThreadAndRunnable/src/module-info.java
@@ -0,0 +1,6 @@
+module ThreadAndRunnable {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+}
---
+++ JavaFX/UIThread/src/module-info.java
@@ -0,0 +1,6 @@
+module UIThread {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+}
