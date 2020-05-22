009277692ca690fd754299015f1eb22df5befe84
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-05-22 13:51:23 +0530

    Sample TODO list Application

---
+++ JavaFX/SampleTodoList/src/com/ashlesh/todolist/DialogController.java
@@ -0,0 +1,34 @@
+package com.ashlesh.todolist;
+
+import com.ashlesh.todolist.datamodel.TodoData;
+import com.ashlesh.todolist.datamodel.TodoItem;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
+
+public class DialogController {
+
+    @FXML
+    private TextField shortDescriptionField;
+
+    @FXML
+    private TextArea detailsArea;
+
+    @FXML
+    private DatePicker deadlinePicker;
+
+    public TodoItem processResults() {
+        String shortDescription = shortDescriptionField.getText().trim();
+        String details = detailsArea.getText().trim();
+        LocalDate deadlineValue = deadlinePicker.getValue();
+
+        TodoItem newItem = new TodoItem(shortDescription, details, deadlineValue);
+        TodoData.getInstance().addTodoItem(newItem);
+        return newItem;
+    }
+}
+
+
---
+++ JavaFX/SampleTodoList/src/module-info.java
@@ -0,0 +1,6 @@
+module SampleTodoList {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens com.ashlesh.todolist;
+}
