02a24755d39ea7d6af6a0b0b03b7ab5322291243
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-05-22 13:48:09 +0530

    Other Layout

---
+++ JavaFX/Hbox/src/module-info.java
@@ -0,0 +1,6 @@
+module Hbox {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens sample;
+}
---
+++ JavaFX/OtherLayouts/src/module-info.java
@@ -0,0 +1,6 @@
+module OtherLayouts {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens sample;
+}
