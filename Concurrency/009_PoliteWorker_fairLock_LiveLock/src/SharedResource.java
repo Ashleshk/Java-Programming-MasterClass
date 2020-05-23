788a9f795505f738de01e32b9b98108715907688
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-05-23 15:46:24 +0530

    JavaFX Background Task

---
+++ Concurrency/009_PoliteWorker_fairLock_LiveLock/src/SharedResource.java
@@ -0,0 +1,16 @@
+public class SharedResource {
+
+    private Worker owner;
+
+    public SharedResource(Worker owner) {
+        this.owner = owner;
+    }
+
+    public Worker getOwner() {
+        return owner;
+    }
+
+    public synchronized void setOwner(Worker owner) {
+        this.owner = owner;
+    }
+}
---
+++ Concurrency/010_JavaFX_bacckground_task/src/module-info.java
@@ -0,0 +1,5 @@
+module JavaFX.bacckground.task {
+    requires javafx.fxml;
+    requires javafx.controls;
+    opens sample;
+}
